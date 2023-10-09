45e3fd7fa2008eda77d4c1c0107f19c1f7444f02
Emil Grużalski <emil.gruzalski@gmail.com>
2023-10-09 14:34:37 +0200

    Add Countdown component.

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
-	import Grid from "./Grid.svelte";
+	import { onMount } from "svelte";
+	import Countdown from "./Countdown.svelte";
+    import Found from "./Found.svelte";
+    import Grid from "./Grid.svelte";
     import { levels } from "./levels";
     import type { Level } from "./levels";
+	import { shuffle } from "./utils";
 
     const level = levels[0];
 
     let size: number = level.size;
     let grid: string[] = create_grid(level);
     let found: string[] = [];
+    let remaining: number = level.duration;
+    let duration: number = level.duration;
+    let playing: boolean = false;
 
     function create_grid(level: Level) {
         const copy = level.emojis.slice();
@@ -23,20 +30,48 @@
 
         pairs.push(...pairs);
 
-        return pairs;
+        return shuffle(pairs);
     }
+
+    function countdown() {
+        const start = Date.now();
+        let remaining_at_start = remaining;
+
+        function loop() {
+            if (playing) return;
+
+            requestAnimationFrame(loop);
+
+            remaining = remaining_at_start - (Date.now() - start);
+
+            if (remaining <= 0) {
+                // TODO game over
+                playing = false;
+            }
+        }
+
+        loop();
+    }
+
+    onMount(countdown);
 </script>
 <div class="game">
     <div class="info">
-
+        <Countdown {remaining} duration={level.duration} />
     </div>
 
     <div class="grid-container">
-        <Grid {grid} />
+        <Grid 
+            {grid} 
+            on:found={(e) => {
+                found = [...found, e.detail.emoji];
+            }}
+            {found}
+        />
     </div>
 
     <div class="info">
-
+        <Found {found} />
     </div>
 </div>
 
@@ -53,12 +88,10 @@
     .info {
         width: 80em;
         height: 10em;
-        background: purple;
     }
 
     .grid-container {
         width: 80em;
         height: 80em;
-        background: teal;
     }
 </style>
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	import { flip } from "svelte/animate";
-
     export let emoji: string;
     export let selected: boolean;
+    export let found: boolean;
 </script>
 
 <div class="square" class:flipped={selected}>
     <button on:click />
-    <span> {emoji} </span>
+
+    {#if !found}
+        <span> {emoji} </span>
+    {/if}
 </div>
 
 <style>
